37b47ba204f97f13f6d4bb75af559793cd19c9ec
Maxim Litvak <maxim.litvak@gmail.com>
2024-07-27 15:29:41 +0200

    [#1] first commit of the splitted project

---
+++ .gitlab/merge_request_templates/default.md
@@ -0,0 +1,18 @@
+## Description of Changes
+
+Please describe the changes you have made. Include the following:
+- What changes did you make?
+- Why did you make these changes?
+- Any additional details or context for the changes?
+
+## Linked Issue
+
+- [ ] I have linked this merge request to an issue. Example: Fixes #123
+
+## Checklist
+
+- [ ] I have checked that my code additions did not fail any tests
+
+## Contribution License Agreement
+
+By submitting this merge request, I acknowledge that any and all contributions to this project are provided under the terms that they are not owned by me. I grant 7cows.io the rights to use, modify, and incorporate these contributions into the project without any restrictions.
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for, make_response
+import os, time, sys
+from flask import session
+import logging
+
+app = Flask(__name__)
+app.secret_key = "dev secret"
+
+app.logger.handlers.clear()
+
+# Set the logging level and format
+app.logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+
+# Create a handler and set the formatter
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+# Add the handler to the logger
+app.logger.addHandler(handler)
+app.config['API_ENDPOINT'] = os.environ.get('API_ENDPOINT', 'https://langtools.io')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pie/')
+@app.route('/pie')
+def proto(proto_lang = 'pie'):
+    proto_lang == 'pie'
+    session['proto_lang'] = proto_lang    
+    selected_language = 'English'  # set the selected language to English by default
+    return render_template('proto.html', debug = app.debug, proto_lang = proto_lang, languages=[("English", 7333), ("German", 1815)], submitted = False, selected_language = selected_language, submitted_value = "")
+
+
+@app.route('/tinder/')
+@app.route('/tinder')
+def engdeu():
+    #conn = get_connection('engdeu')
+    #conn.close()
+    return render_template('engdeu.html', proto_lang  = 'tinder', languages = [('English', 4626), ('German', 4626)], submitted = False, selected_language = 'English', submitted_value = "")
